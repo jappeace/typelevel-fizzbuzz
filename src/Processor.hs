899fd55dd7547316944364b54594494b2d5ff9c8
Jappie Klooster <jappieklooster@hotmail.com>
2021-09-07 22:16:46 +0200

    Trying to figure out OR

--- src/Processor.hs
+++ src/Processor.hs
@@ -42,9 +42,10 @@ notProof2 = nil
 
 -- type alias didn't work with partial application.
 -- guess we just use more typeclasses
-class And input input2 ultout
+class And (input :: Axiom) (input2 :: Axiom) (out :: Axiom) | input input2 -> out
 instance (Not x out, Nand input input2 x) =>
-  And input input2 out -- note that we're not even a fundep
+  And input input2 out
+        -- note that we're not even a fundep
 
 andProof1 :: And Truth Truth Truth => b
 andProof1 = nil
@@ -55,10 +56,28 @@ andProof3 = nil
 andProof4 :: And Truth Lies Lies => b
 andProof4 = nil
 
-x :: Int
-x = notProof1
+class Or input input2 out
+instance (And input input2 xin, Nand xin input2 out) =>
+  Or input input2 out
+
+
+orProof1 :: Or Truth Truth Truth => b
+orProof1 = nil
+orProof2 :: Or Lies Truth Truth => b
+orProof2 = nil
+orProof3 :: Or Lies Lies Lies => b
+orProof3 = nil
+orProof4 :: Or Truth Lies Truth => b
+orProof4 = nil
+
+x :: Int -- inhabitation means proved
+x = notProof1 -- since the values are irrelevant, we jam all proofs here
     notProof2
     andProof1
     andProof2
     andProof3
     andProof4
+    orProof1
+    orProof2
+    orProof3
+    orProof4
